077dcd17bbaa473d11b0d0186dbc79a6625674fa
Teresa Pham <79329784+teresa-c-pham@users.noreply.github.com>
2024-04-05 04:08:15 +0000

    Finalize Lunch and Dinner Classes and Tests

--- inventory.rb
+++ inventory.rb
@@ -2,7 +2,9 @@ class Inventory
 
 end
 
-class Appetizer
+
+# Distinguish Lunch Menu Items from Dinner Menu Items
+class Lunch
   attr_accessor :name
   attr_accessor :cost
   attr_accessor :amount
@@ -45,18 +47,45 @@ class Appetizer
   end
 end
 
-class Entree
-  attr_accessor :pho
-  attr_accessor :fried_rice
-  attr_accessor :banh_mi
-  attr_accessor :salt_pepper_tofu
-  attr_accessor :orange_chicken
-  attr_accessor :korean_short_ribs
-end
+class Dinner
+  attr_accessor :name
+  attr_accessor :cost
+  attr_accessor :amount
 
-class Dessert
-  attr_accessor :che
-  attr_accessor :matcha_crepe
-  attr_accessor :cheesecake
-  attr_accessor :mochi_ice_cream
+  def initialize(name, amount, cost)
+    self.name = name
+    self.amount = amount
+    self.cost = cost
+  end
+
+  def name=(value)
+    # Check to see if name is a string
+    unless value.is_a?(String)
+      raise TypeError, 'Appetizer name must be a string'
+    end
+
+    @name = value.capitalize
+  end
+
+  def amount=(value)
+    # Check to see if amount is an integer
+    unless value.is_a?(Integer)
+      raise TypeError, 'Appetizer amount must be an integer'
+    end
+
+    @amount = value
+  end
+
+  def cost=(value)
+    # Check to see if cost is a float
+    unless value.is_a?(Float)
+      raise TypeError, 'Appetizer cost must be a float'
+    end
+
+    @cost = value.round(2)
+  end
+
+  def total_cost
+    return amount * cost
+  end
 end
--- test/test_Inventory.rb
+++ test/test_Inventory.rb
@@ -1,32 +1,62 @@
 require 'minitest/autorun'
 require './inventory.rb'
 
-class TestInventory < Minitest::Test
-  def test_appetizer_initialize
-    shrimp = Appetizer.new("Firecracker Shrimp", 6, 3.50)
+class TestLunchInventory < Minitest::Test
+  def test_lunch_initialize
+    shrimp = Lunch.new("Firecracker Shrimp", 6, 3.50)
     assert_equal 6, shrimp.amount, "Set Firecracker Shrimp amount method failed"
   end
 
-  def test_appetizer_change_cost
-    calamari = Appetizer.new("Calamari", 10, 5.25)
+  def test_lunch_change_cost
+    calamari = Lunch.new("Calamari", 10, 5.25)
     calamari.cost = 6.25
     assert_equal 6.25, calamari.cost, "Set Calamari cost method failed"
   end
 
-  def test_appetizer_change_name
-    miso = Appetizer.new("Miso", 10, 5.25)
+  def test_lunch_change_name
+    miso = Lunch.new("Miso", 10, 5.25)
     miso.name = "miso soup"
     assert_equal "Miso soup", miso.name, "Set Miso Soup name method failed"
   end
 
-  def test_appetizer_change_amount
-    crab = Appetizer.new("crab rangoon", 5, 3.00)
+  def test_lunch_change_amount
+    crab = Lunch.new("crab rangoon", 5, 3.00)
     crab.amount = 8
     assert_equal 8, crab.amount, "Set Crab Rangoon amount method failed"
   end
 
-  def test_appetizer_total_cost
-    eggroll = Appetizer.new("eggroll", 5, 0.65)
+  def test_lunch_total_cost
+    eggroll = Lunch.new("eggroll", 5, 0.65)
+    assert_equal 3.25, eggroll.total_cost, "Total cost method failed"
+  end
+end
+
+class TestDinnerInventory < Minitest::Test
+  def test_dinner_initialize
+    pho = Dinner.new("Pho", 1, 8.50)
+    assert_equal 8.5, pho.cost, "Initialize Dinner method failed"
+  end
+
+  def test_dinner_change_cost
+    chicken = Dinner.new("orange Chicken", 10, 7.25)
+    chicken.cost = 6.75
+    assert_equal 6.75, chicken.cost, "Set Dinner cost method failed"
+  end
+
+  def test_dinner_change_name
+    lobster = Dinner.new("lobster", 10, 5.25)
+    lobster.name = "lobster tails"
+    assert_equal "Lobster tails", lobster.name, "Set Dinner name method failed"
+  end
+
+  def test_dinner_change_amount
+    noodles = Dinner.new("stir-fry noodles", 1, 6.00)
+    noodles.amount = 2
+    assert_equal 2, noodles.amount, "Set Dinner amount method failed"
+  end
+
+  def test_dinner_total_cost
+    eggroll = Dinner.new("eggroll", 5, 0.65)
     assert_equal 3.25, eggroll.total_cost, "Total cost method failed"
   end
 end
