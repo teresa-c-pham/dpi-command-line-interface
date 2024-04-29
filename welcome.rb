4b5a87fe556191460f60e17b3c34bc286282ac48
Teresa Pham <79329784+teresa-c-pham@users.noreply.github.com>
2024-04-29 19:22:50 +0000

    Implement welcome menu options

--- welcome.rb
+++ welcome.rb
@@ -1,13 +1,70 @@
 require 'time'
 
+def clock_in
+  puts "Enter your Employee ID:"
+  id = gets.chomp
+  puts "Employee #{id} has clocked in."
+end
+
+def clock_out
+  puts "Enter your Employee ID:"
+  id = gets.chomp
+  puts "Employee #{id} has clocked out."
+end
+
+def take_order
+  puts "Guest Name:"
+  name = gets.chomp
+
+end
+
+def update_inventory
+  puts "Enter Inventory Item Name:"
+  item = gets.chomp
+
+end
+
+
 def welcome
-  pp "Welcome to Miso Restaurant's Employee Portal!"
-  pp "Today is #{Time.now().strftime "%d/%m/%Y %H:%M"}"
-  pp "How can I help you today?"
-  pp "1. Clock in/out for work."
-  pp "2. Take a guest order."
-  pp "3. Update Inventory."
-  pp "4. Exit."
+  puts "Welcome to Miso Restaurant's Employee Portal!"
+  puts "Today is #{Time.now().strftime "%d/%m/%Y %H:%M"}"
+  puts "How can I help you today?"
+  puts "1. Clock in for work."
+  puts "2. Clock out for work."
+  puts "3. Take a guest order."
+  puts "4. Update Inventory."
+  puts "5. Exit."
+
+  input = gets.chomp
+  while input
+    case input
+    when "1"
+      clock_in
+    when "2"
+      clock_out
+    when "3"
+      take_order
+    when "4"
+      update_inventory
+    when "5"
+      puts "Good bye!"
+      exit!
+    else
+      puts "Wrong input. Please select a valid option"
+    end
+
+    # Repeat the welcome screen prompt
+    puts ""
+    puts "Welcome to Miso Restaurant's Employee Portal!"
+    puts "Today is #{Time.now().strftime "%d/%m/%Y %H:%M"}"
+    puts "How can I help you today?"
+    puts "1. Clock in for work."
+    puts "2. Clock out for work."
+    puts "3. Take a guest order."
+    puts "4. Update Inventory."
+    puts "5. Exit."
+    input = gets.chomp
+  end
 end
 
 welcome
