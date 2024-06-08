7be020de91e8b3224dcf1e3d2fc6766903660a4b
Teresa Pham <79329784+teresa-c-pham@users.noreply.github.com>
2024-06-08 20:10:54 +0000

    Add take order function

--- welcome.rb
+++ welcome.rb
@@ -1,75 +1,73 @@
 require 'time'
+require './order.rb'
+require './inventory.rb'
 
-# TODO
-# clock_in
-# clock_out
-# take_order
-# clock_in employee through Employees class
-# function to display guest's order
+class Welcome
+  attr_accessor :items
 
-def clock_in
-  puts "Enter your Employee ID:"
-  id = gets.chomp
-  puts "Employee #{id} has clocked in."
-end
+  def initialize
+    lunch = [Item.new("Firecracker Shrimp", 6, 3.50), Item.new("Calamari", 10, 5.25)]
+    dinner = [Item.new("crab rangoon", 5, 3.00), Item.new("Miso", 10, 5.25)]
 
-def clock_out
-  puts "Enter your Employee ID:"
-  id = gets.chomp
-  puts "Employee #{id} has clocked out."
-end
-
-def take_order
-  puts "Guest Name:"
-  name = gets.chomp
-  order = Order.new(name)
-
-  # List available menu items for designated time: Lunch/Dinner
-  dinner = Time.parse "3:00 pm"
-  if Time.now < dinner
-    items = inventory.lunch_items
-  else
-    items = inventory.dinner_items
-  end
-  
-end
-
-def update_inventory
-  puts "Enter Inventory Item Name:"
-  item = gets.chomp
-end
-
-
-def welcome
-  puts "Welcome to Miso Restaurant's Employee Portal!"
-  puts "Today is #{Time.now().strftime "%d/%m/%Y %H:%M"}"
-  puts "How can I help you today?"
-  puts "1. Clock in for work."
-  puts "2. Clock out for work."
-  puts "3. Take a guest order."
-  puts "4. Update Inventory."
-  puts "5. Exit."
-
-  input = gets.chomp
-  while input
-    case input
-    when "1"
-      clock_in
-    when "2"
-      clock_out
-    when "3"
-      take_order
-    when "4"
-      update_inventory
-    when "5"
-      puts "Good bye!"
-      exit!
+    # List available menu items for designated time: Lunch/Dinner
+    dinner_time = Time.parse "3:00 pm"
+    if Time.now < dinner_time
+      self.items = lunch
     else
-      puts "Wrong input. Please select a valid option"
+      self.items = dinner
     end
+  end
+  # TODO
+  # clock_in
+  # clock_out
+  # take_order
+  # clock_in employee through Employees class
+  # function to display guest's order
 
-    # Repeat the welcome screen prompt
-    puts ""
+  def clock_in
+    puts "Enter your Employee ID:"
+    id = gets.chomp
+    puts "Employee #{id} has clocked in."
+  end
+
+  def clock_out
+    puts "Enter your Employee ID:"
+    id = gets.chomp
+    puts "Employee #{id} has clocked out."
+  end
+
+  def take_order
+    puts "Guest Name:"
+    name = gets.chomp
+    order = Order.new(name)
+
+    puts "What would you like to order?"
+    option = ""
+    while option != "0"
+      count = 1
+      self.items.each do |item|
+        puts "#{count}. #{item.name} : #{item.cost}"
+        count += 1
+      end
+      option = gets.chomp
+      if option != "0" && option.to_i < self.items.length
+        order.add_item(self.items[option.to_i - 1])
+      end
+
+      puts "Your order:"
+      order.items.each do |item|
+        puts "#{item.name} : #{item.cost}"
+      end
+    end
+  end
+
+  def update_inventory
+    puts "Enter Inventory Item Name:"
+    item = gets.chomp
+  end
+
+
+  def welcome
     puts "Welcome to Miso Restaurant's Employee Portal!"
     puts "Today is #{Time.now().strftime "%d/%m/%Y %H:%M"}"
     puts "How can I help you today?"
@@ -78,14 +76,39 @@ def welcome
     puts "3. Take a guest order."
     puts "4. Update Inventory."
     puts "5. Exit."
+
     input = gets.chomp
+    while input
+      case input
+      when "1"
+        clock_in
+      when "2"
+        clock_out
+      when "3"
+        take_order
+      when "4"
+        update_inventory
+      when "5"
+        puts "Good bye!"
+        exit!
+      else
+        puts "Wrong input. Please select a valid option"
+      end
+
+      # Repeat the welcome screen prompt
+      puts ""
+      puts "Welcome to Miso Restaurant's Employee Portal!"
+      puts "Today is #{Time.now().strftime "%d/%m/%Y %H:%M"}"
+      puts "How can I help you today?"
+      puts "1. Clock in for work."
+      puts "2. Clock out for work."
+      puts "3. Take a guest order."
+      puts "4. Update Inventory."
+      puts "5. Exit."
+      input = gets.chomp
+    end
   end
 end
 
-
-
-
-
-
-
-welcome
+start = Welcome.new
+start.welcome
